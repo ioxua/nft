0e97fbb8575d3b44675c3573742255d3c935759d
Yehoshua Oliveira <ioxua@newgo.com.br>
2022-02-10 23:35:00 -0300

    yea I like abstraction how do you know

--- script.py
+++ script.py
@@ -1,3 +1,24 @@
-from PIL import Image, ImageDraw
-img = Image.new(mode, size, color)
-img.save(filename)
+from PIL import Image, ImageDraw, ImageFont
+
+size = (500,500)
+
+# ideas: random bg/font color, with no regard for contrast
+# how to list all characters from a ttf file?
+
+def render_chars_from_font(font_file, size=16):
+    font = ImageFont.truetype("./fonts/Roboto-Regular.ttf", size)
+    render_character('a', font)
+
+
+def render_character(char, font):
+    """
+    thank you https://stackoverflow.com/a/1970930
+    """
+
+
+img = Image.new('RGB', size, color = (0, 0, 0))
+ 
+d = ImageDraw.Draw(img)
+d.text((10,10), "Hello World", fill=(255,255,255))
+ 
+img.save('pil_text.png')
