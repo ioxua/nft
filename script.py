6e9ded4df68447c69835dc86d9b44e9c80b6a44a
Yehoshua Oliveira <ioxua@newgo.com.br>
2022-02-10 23:37:41 -0300

    just had an idea. EVERYTHING IS CRYPTO

--- script.py
+++ script.py
@@ -1,6 +1,8 @@
 from PIL import Image, ImageDraw, ImageFont
 
-size = (500,500)
+crypto_width = 500
+crypto_height = 500
+crypto_size = (crypto_width, crypto_height)
 
 # ideas: random bg/font color, with no regard for contrast
 # how to list all characters from a ttf file?
@@ -10,15 +12,17 @@ def render_chars_from_font(font_file, size=16):
     render_character('a', font)
 
 
-def render_character(char, font):
+def render_character(crypto_char, font):
     """
     thank you https://stackoverflow.com/a/1970930
     """
 
 
-img = Image.new('RGB', size, color = (0, 0, 0))
- 
-d = ImageDraw.Draw(img)
-d.text((10,10), "Hello World", fill=(255,255,255))
- 
-img.save('pil_text.png')
+    crypto_img = Image.new('RGB', crypto_size, color = (0, 0, 0))
+    
+    crypto_draw = ImageDraw.Draw(crypto_img)
+
+    crypto_text_size = crypto_draw.textsize(crypto_char, font=font)
+    crypto_draw.text(((W-w)/2,(H-h)/2), crypto_char, fill="black")
+    
+    crypto_img.save('pil_text.png')
